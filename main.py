019822badae95a82268f98b8b4c293f858a95333
geunseoul2 <geunseoul2@gmail.com>
2023-10-18 09:59:17 +0000

    even_list finished:

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ def even_list(int_list: List[int]) -> List[int]:
     A list of even integers.
     """
     # TODO: Implement even_list
+    even_numbers = []
+
+    for num in int_list:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    
+    return even_numbers
     pass
 
 # Skeleton code for sum_of_squares_of_even
